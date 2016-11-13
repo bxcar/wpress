a4bc7ef23226a1ce2004c368b7e7254a8caaa9ee
bxcar <malanchukdima@mail.ru>
2016-11-13 14:48:02 +0200

    some fixes

--- wp-content/themes/sydney/header.php
+++ wp-content/themes/sydney/header.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic" rel="stylesheet">
     <?php if (!function_exists('has_site_icon') || !has_site_icon()) : ?>
         <?php if (get_theme_mod('site_favicon')) : ?>
             <link rel="shortcut icon" href="<?php echo esc_url(get_theme_mod('site_favicon')); ?>"/>
